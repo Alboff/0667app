fcd07a7d988040dd5f8931c209fb5a2a598a72f3
Alex <albo@inbox.ru>
2022-08-29 16:16:12 +0300

    add profile

--- src/Friends.jsx
+++ src/Friends.jsx
@@ -1,10 +1,8 @@
-const h2style = {
-  color: "blue",
-  fontWeight: "700",
-};
+
 
 const Friends = () => {
-  return <h2 style={h2style}>Это страница с друзьями</h2>;
+  return (<div className="row"></div>
+  <h2 style={h2style}>Это страница с друзьями</h2>;
 };
 
 export default Friends;
--- src/state.jsx
+++ src/state.jsx
@@ -8,6 +8,18 @@ const user = {
     "https://dm.henkel-dam.com/is/image/henkel/men_perfect_com_thumbnails_home_pack_400x400-wcms-international?scl=1&fmt=jpg",
 };
 
+const users = {
+  0: {name: "Владислава", lastname: "Анютина"},
+  1: {name: "Юлия", lastname: "Петрова"},
+  2: {name: "Петр", lastname: "блочков"},
+  3: {name: "Константин", lastname: "Лесков"},
+  4: {name: "Парфирий", lastname: "Тетерин"},
+};
+
+export function getUsers() {
+  return user;
+}
+
 export function getUser() {
   return user;
 }

8671de28d3a3569bd314e6adb6851ab853455493
Alex <albo@inbox.ru>
2022-09-01 10:38:31 +0300

    last

--- src/Friends.jsx
+++ src/Friends.jsx
@@ -1,42 +1,60 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
 const TableRow = (props) => {
   return (
     <tr>
       <th scope="row">{props.index + 1}</th>
-      <td>{props.name}</td>
-      <td>{props.lastname}</td>
+      <td>
+        <NavLink to={"/profile/" + props.id}>
+          {props.name} {props.lastname}
+        </NavLink>
+      </td>
     </tr>
   );
 };
 
-const Friends = (props) => {
-  let users = props.function();
-  let usersCount = Object.keys(users).length;
-  let userRow = [];
+class Friends extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { userRow: [] };
+  }
 
-  for (let i = 0; i < usersCount; i++) {
-    userRow.push(
-      <TableRow
-        index={i}
-        key={i}
-        name={users[i].name}
-        lastname={users[i].lastname}
-      />
+  componentDidMount() {
+    this.props.function().then((users) => {
+      console.log(users);
+      let usersCount = users.length;
+      let userRow = [];
+
+      for (let i = 0; i < usersCount; i++) {
+        userRow.push(
+          <TableRow
+            id={users[i].id}
+            index={i}
+            key={i}
+            name={users[i].name}
+            lastname={users[i].lastname}
+          />
+        );
+      }
+      this.setState({ userRow: userRow });
+    });
+  }
+  render() {
+    return (
+      <div className="row">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">№</th>
+              <th scope="col">Имя и Фамилия</th>
+            </tr>
+          </thead>
+          <tbody>{this.state.userRow}</tbody>
+        </table>
+      </div>
     );
   }
-  return (
-    <div className="row">
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Имя</th>
-            <th scope="col">Фамилия</th>
-          </tr>
-        </thead>
-        <tbody>{userRow}</tbody>
-      </table>
-    </div>
-  );
-};
+}
 
 export default Friends;
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,20 +1,21 @@
 import styles from "./Profile.module.css";
 
 const Profile = (props) => {
-  let user = props.function();
+  let userId = window.location.pathname.split("/")[2];
+  let user = props.function(userId);
   console.log(user);
+  console.log(userId);
   return (
     <div className="row">
       <div className="col-sm-4">
-        <img className="img-fluid" src="user.avatar" alt="" />
+        <img className="img-fluid" src={user.avatar} alt="" />
       </div>
       <div className="col-sm-8">
         <h1 className={styles.about}>
-          {user.name}
-          {user.lastname}
+          {user.name} {user.lastname}
         </h1>
         <h2 style={{ backgroundColor: "lightblue" }}>{user.about}</h2>
-        <h3>E-mail</h3>
+        <h3>E-mail: {user.email}</h3>
         <p className={styles.id}>Id: {user.id}</p>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex saepe
